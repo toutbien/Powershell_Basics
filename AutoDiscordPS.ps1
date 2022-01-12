07b748339f0228d316d3215e7799d5a8c2452db4
Lisa <90510480+toutbien@users.noreply.github.com>
2022-01-12 09:47:22 -0500

    Update AutoDiscordPS.ps1
    altered path names to be general - in CAPS if needs attention

--- AutoDiscordPS.ps1
+++ AutoDiscordPS.ps1
@@ -1,7 +1,7 @@
 #time automation script
 $TaskName = 'MyScript'
-$User= "C:Users\toutb\"
-$ScriptPath = "C:\Users\toutbien\contoso\DiscordHelloPS.ps1"
+$User= "C:Users\USERNAME\"
+$ScriptPath = "C:\Users\USERNAME\FILEPATH\DiscordHelloPS.ps1"
 
 $Trigger= New-ScheduledTaskTrigger -At 12:30 -Daily 
 $Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-executionpolicy bypass -noprofile -file $ScriptPath" 
